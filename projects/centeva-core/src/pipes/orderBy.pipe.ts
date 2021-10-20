cbb24931552149dababace545266956733b949f1
Robert Emmett <robert.emmett@cen.centeva.com>
2021-10-20 15:56:16 -0600

    Updated version number

--- projects/centeva-core/src/pipes/orderBy.pipe.ts
+++ projects/centeva-core/src/pipes/orderBy.pipe.ts
@@ -6,6 +6,7 @@ export enum SortOrder { Ascending = "asc", Descending = "desc" };
 @Pipe({ name: 'orderBy' })
 export class OrderByPipe implements PipeTransform {
     transform(collection: any[], iteratees: string | string[], orders?: SortOrder | SortOrder[]) {
+        if (!collection) return collection;
         return _.orderBy(collection, iteratees, orders);
     }
 }
