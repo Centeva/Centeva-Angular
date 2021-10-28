df40be84e472c7abf786dd8b027cd11b3243bc17
Robert Emmett <robert.emmett@cen.centeva.com>
2021-10-27 18:00:30 -0600

    Changed orderBy to be case insensitive for string items

--- projects/centeva-core/src/pipes/orderBy.pipe.ts
+++ projects/centeva-core/src/pipes/orderBy.pipe.ts
@@ -7,6 +7,11 @@ export enum SortOrder { Ascending = "asc", Descending = "desc" };
 export class OrderByPipe implements PipeTransform {
     transform(collection: any[], iteratees: string | string[], orders?: SortOrder | SortOrder[]) {
         if (!collection) return collection;
+        for (let i = 0; i < collection.length; ++i) {
+            if (typeof collection[i] === 'string')
+                collection[i] = (collection[i] as string).toLowerCase();
+        }
+
         return orderBy(collection, iteratees, orders);
     }
 }
